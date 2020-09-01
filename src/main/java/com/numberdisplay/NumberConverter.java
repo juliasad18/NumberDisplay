03f3dcab20d6fb639fa29216ba99e1b37992fbd2
Julija Asadciha <julija.asadciha@cognizant.com>
2020-09-01 09:29:11 +0300

    Renamed methods to: convertNumber() and numberVocabulary(); Made numberVocabulary() as private static; Created private constructor to prevent creation of NumberConverter instance; Made convertNumber(INumber newNumber) throws NullPointerException (in case if wrong value is given to input); Added condition to convertNumber() which will check is given input value null or not null.

--- src/main/java/com/numberdisplay/NumberConverter.java
+++ src/main/java/com/numberdisplay/NumberConverter.java
@@ -4,33 +4,39 @@ import java.util.HashMap;
 
 public class NumberConverter {
 
-    public static String[] displayNumber (String number) {
-        String firstRowString = "";
-        String secondRowString = "";
-        String thirdRowString = "";
-
-        Number newNumber = new Number(number);
-
-        StringBuilder builder1 = new StringBuilder();
-        StringBuilder builder2 = new StringBuilder();
-        StringBuilder builder3 = new StringBuilder();
-
-
-        for (int i = 0; i < newNumber.getNumber().length(); i++) {
-            int numberFromChar = Character.getNumericValue(newNumber.getNumber().charAt(i));
-            HashMap numberHashMap = findNumber(numberFromChar);
-
-            firstRowString = builder1.append(" ").append(numberHashMap.get(1).toString()).toString();
-            secondRowString = builder2.append(" ").append(numberHashMap.get(2).toString()).toString();
-            thirdRowString = builder3.append(" ").append(numberHashMap.get(3).toString()).toString();
-
-        }
-
-        return new String[] {firstRowString, secondRowString, thirdRowString};
-
+    private NumberConverter() {
     }
 
-    public static HashMap<Integer, String> findNumber (int number) {
+    public static void convertNumber (INumber newNumber) throws NullPointerException {
+        if (!(newNumber.getNumber().isEmpty())) {
+            String firstRowString = "";
+            String secondRowString = "";
+            String thirdRowString = "";
+
+            StringBuilder builder1 = new StringBuilder();
+            StringBuilder builder2 = new StringBuilder();
+            StringBuilder builder3 = new StringBuilder();
+
+            for (int i = 0; i < newNumber.getNumber().length(); i++) {
+                int numberFromChar = Character.getNumericValue(newNumber.getNumber().charAt(i));
+                HashMap numberHashMap = numberVocabulary(numberFromChar);
+
+                firstRowString = builder1.append(" ").append(numberHashMap.get(1).toString()).toString();
+                secondRowString = builder2.append(" ").append(numberHashMap.get(2).toString()).toString();
+                thirdRowString = builder3.append(" ").append(numberHashMap.get(3).toString()).toString();
+            }
+
+            newNumber.appendToDisplayedNumber(firstRowString, secondRowString, thirdRowString);
+            newNumber.printDisplayedNumber();
+
+        } else {
+            System.out.println("Inserted value is empty. Please write any number.");
+        }
+    }
+
+
+
+    private static HashMap<Integer, String> numberVocabulary (int number) {
 
         HashMap<Integer, String> hashMap = new HashMap<>();
 
