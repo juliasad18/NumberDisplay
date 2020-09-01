bc5e3af3196f608d9592d800cd09d1ed80d54e19
Julija Asadciha <julija.asadciha@cognizant.com>
2020-09-01 09:34:04 +0300

    Mode Scanner in as private static; Added try-catch block to catch NullPointerException error in case if entered value is not number but character; Modified conditions for checking do we need to continue running the app or quit.

--- src/main/java/com/numberdisplay/Application.java
+++ src/main/java/com/numberdisplay/Application.java
@@ -1,27 +1,34 @@
 package com.numberdisplay;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Application {
+    private static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         System.out.println("Entrez le numéro: ");
-        Scanner in = new Scanner(System.in);
 
         while(true) {
             String inputNumber = in.nextLine();
 
-            String[] numberArray = NumberConverter.displayNumber(inputNumber);
-            for(String string : numberArray) {
-                System.out.println(string);
+            try {
+                Long start = System.currentTimeMillis();
+                NumberConverter.convertNumber(new Number(inputNumber));
+                System.out.println("Logging time to check performance --> NumberConverter.convertNumber() working time: " + (System.currentTimeMillis() - start) + " milliseconds.");
+            } catch(NullPointerException e) {
+                Logger logger = Logger.getLogger(Number.class.getName());
+                logger.warning("Error message: " + e.toString());
+                System.out.println("Inserted value must be a number.");
             }
 
             System.out.println("Voulez-vous entrer un nouveau numéro? (Oui/Non)");
             String inputAnswer = in.nextLine();
-            if (inputAnswer.equals("Non")) {
+            if (inputAnswer.toLowerCase().contains("non")) {
                 System.out.println("Au revoir!");
                 break;
-            } else if (inputAnswer.equals("Oui")) {
+            } else if (inputAnswer.toLowerCase().contains("oui")) {
                 System.out.println("Entrez le numéro: ");
             } else {
                 System.out.println("Considered as Yes :)");
