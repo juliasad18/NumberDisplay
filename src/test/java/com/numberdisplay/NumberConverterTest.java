04f436a8b8810599092bacf3e7e0c305015336d6
Julija Asadciha <julija.asadciha@cognizant.com>
2020-09-01 09:37:47 +0300

    Added new testcase for checking that if character is given to input - NullPointerException is thrown; Mocked Number class to make testing of convertNumber() focused only on itself.

--- src/test/java/com/numberdisplay/NumberConverterTest.java
+++ src/test/java/com/numberdisplay/NumberConverterTest.java
@@ -4,20 +4,21 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberConverterTest {
-    NumberConverter numberConverter = new NumberConverter();
 
     @Test
-    public void displayNumberTest() {
-        Number testNumber = new Number("0123456789");
-        String[] actualDisplay = numberConverter.displayNumber(testNumber.getNumber());
+    public void testNumberConverter_convertNumber_validInput() {
+        NumberMock numberMock = new NumberMock("0123456789");
+        NumberConverter.convertNumber(numberMock);
 
-        assertEquals(" ._. ... ._. ._. ... ._. ._. ._. ._. ._.", actualDisplay[0], "1st row is incorrect");
-        assertEquals(" |.| ..| ._| ._| |_| |_. |_. ..| |_| |_|", actualDisplay[1], "2nd row is incorrect");
-        assertEquals(" |_| ..| |_. ._| ..| ._| |_| ..| |_| ._|", actualDisplay[2], "3rd row is incorrect");
-
+        assertEquals(" ._. ... ._. ._. ... ._. ._. ._. ._. ._.", numberMock.getFirstRow(), "Actual String doesn't match the expected result.");
+        assertEquals(" |.| ..| ._| ._| |_| |_. |_. ..| |_| |_|", numberMock.getSecondRow(), "Actual String doesn't match the expected result.");
+        assertEquals(" |_| ..| |_. ._| ..| ._| |_| ..| |_| ._|", numberMock.getThirdRow(), "Actual String doesn't match the expected result.");
     }
 
-
-
+    @Test(expected = NullPointerException.class)
+    public void testNumberConverter_convertNumber_invalidInput() {
+        NumberMock numberMock = new NumberMock("ABC");
+        NumberConverter.convertNumber(numberMock);
+    }
 
 }
